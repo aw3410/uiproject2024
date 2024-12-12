836e226c2c8f18c48013d05cd547de9f121661d5
Annika Wang <aw3410@columbia.edu>
2024-12-12 17:13:53 -0500

    added sidebar collapsable to coursepage

--- static/coursepage_two.js
+++ static/coursepage_two.js
@@ -11,10 +11,50 @@ function advancedsearch() {
     }
 }
 
+function professorcollapsible() {
+    var professors = $('#listofprofessors')
+    var toggletext = document.getElementById('professorbutton')
+
+    const isVisible = professors.hasClass('hidden');
+
+    if (isVisible) {
+        professors.removeClass('hidden');
+        toggletext.innerHTML = 'Professors -'
+
+    } else {
+        professors.addClass('hidden');
+        toggletext.innerHTML = 'Professors +'
+    }
+}
+
+function coursecollapsible() {
+    var courses = $('#listofcourses')
+    var toggletext = document.getElementById('coursebutton')
+
+    const isVisible = courses.hasClass('hidden');
+
+    if (isVisible) {
+        courses.removeClass('hidden');
+        toggletext.innerHTML = 'Courses -'
+
+    } else {
+        courses.addClass('hidden');
+        toggletext.innerHTML = 'Courses +'
+    }
+}
 
 $(document).ready(function () {
     $('#filterbutton').click(function () {
         console.log("Filter button clicked!");
         advancedsearch();
     });
+
+    $('#coursebutton').click(function () {
+        coursecollapsible();
+    });
+
+    $('#professorbutton').click(function () {
+        professorcollapsible();
+    });
+
 });
