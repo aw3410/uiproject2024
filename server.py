428ac8ff4a926256e595e37f25cb8a0dc0e0d908
Anisha Gupta <anisha1999.gupta@gmail.com>
2024-12-12 22:24:44 -0500

    added GET APIs for professor and courses, added json data

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask,jsonify,abort,render_template
+import json
 
 app = Flask(__name__)
 
+def load_course_data():
+    with open('static/courseinfo.json') as f:
+        return json.load(f)
+
+def load_professor_course_data():
+    with open('static/professorinfo.json') as f:
+        return json.load(f)
+
+course_data = load_course_data()
+professor_course_data = load_professor_course_data()
+    
 @app.route('/')
 def home():
     return render_template('homepage.html')
@@ -17,6 +29,25 @@ def professorpage():
 @app.route('/course')
 def coursepage(): 
     return render_template('coursepage_two.html')
+
+@app.route('/courses/<coursecode>')
+def courseinfo(coursecode):
+    course = next((course for course in course_data if course['courseCode'] == coursecode), None)
+    
+    if course is None:
+        abort(404, description="Course not found")
+    
+    return jsonify(course)
+
+@app.route('/professors/<profname>')
+def profcourseinfo(profname):
+    prof = next((prof for prof in professor_course_data if prof['professorName'] == profname), None)
+    
+    if prof is None:
+        abort(404, description="Professor not found")
+    
+    return jsonify(prof)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
